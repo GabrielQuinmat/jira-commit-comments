6424520f0e4e5b7d8dfbb28eefc2f08e52b2124a
Gabriel Quintanar <gquintanar@outlook.com>
2024-05-13 19:08:04 -0600

    fix: change repo_path to test

--- tests/test_git_extractor.py
+++ tests/test_git_extractor.py
@@ -5,7 +5,7 @@ from git_extractor import GitExtractor
 
 (
     GitExtractor(True).extract_commit_data(
-        'C:/Users/gquintanar/Downloads/Repos/quantum-ip-video-jobs',
+        'C:/Users/gquin/Downloads/Repos/jira-commit-comments',
         'tests/test_result.json'
     )
 )
