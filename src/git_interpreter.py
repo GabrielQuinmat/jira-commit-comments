0dcc15fb2f93313d9ba23a6587f953b82f991565
Gabriel Quintanar <gquintanar@outlook.com>
2024-05-23 21:00:16 -0600

    fix: fix implementation as needs to use a set to avoid repetition

--- src/git_interpreter.py
+++ src/git_interpreter.py
@@ -19,7 +19,7 @@ class GitInterpreter:
         "Your job is to interpret and summarize the changes made in a list of commits."
         "The text generated should use a formal, technical, but concise and simople style."
         "The text should use a first person plural pronoun (we) to refer to the team or the project."
-        "Don't provide specific dates or hours, neither path of the files, instead interpret the hours worked or the files changed."
+        "Don't provide specific dates or timestamp, neither path of the files, instead interpret the hours worked or the files changed."
         "If the amount of changes is represent the work done for one day, use paragraph or more for the final text. If not, use a sentence or two."
         "We have provided an existing summary up to a certain point: {existing_answer}\n"
         "We have the opportunity to refine the existing summary"
@@ -74,11 +74,12 @@ class GitInterpreter:
             docs = self.loader.load()
             if not docs:
                 return -1
-            print(docs[0])
-            branches = [doc["branch"] for doc in docs]
+            self.log.info(f"Loaded {len(docs)} documents from {self.json_path}")
+            branches = set([doc.metadata["branch"] for doc in docs])
             branch_results = []
             for branch in branches:
-                docs_branch = [doc for doc in docs if doc["branch"] == branch]
+                self.log.info(f"Interpreting branch: {branch}")
+                docs_branch = [doc for doc in docs if doc.metadata["branch"] == branch]
                 splits = self.splitter.split_documents(docs_branch)
                 chain = load_summarize_chain(
                     llm=self.llm,
@@ -90,7 +91,8 @@ class GitInterpreter:
                     output_key="output_text",
                 )
                 result = {branch: chain({"input_documents": splits}, return_only_outputs=True)["output_text"]}
-            return branch_results.append(result)
+                branch_results.append(result)
+            return branch_results
         except Exception as e:
             self.log.error(e)
             print_exception(e)
